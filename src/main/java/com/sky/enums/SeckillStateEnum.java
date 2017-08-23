af82d907661db1c90bb20e43f7ce8bd11f4f4ecd
sky <silence.sky@outlook.com>
2017-08-23 19:38:31 +0800

    add controller

--- src/main/java/com/sky/enums/SeckillStateEnum.java
+++ src/main/java/com/sky/enums/SeckillStateEnum.java
@@ -4,7 +4,8 @@ public enum SeckillStateEnum {
     SUCCESS(1, "秒杀成功"),
     END(0, "秒杀结束"),
     REPART_KILL(-1, "重复异常"),
-    DATA_REWRITE(-3,"数据篡改");
+    DATA_REWRITE(-2, "数据篡改"),
+    INNER_ERROR(-3, "内部错误");
 
     private int state;
     private String stateInfo;
